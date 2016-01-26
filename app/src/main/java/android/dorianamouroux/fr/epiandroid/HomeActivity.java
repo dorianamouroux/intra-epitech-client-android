9c21029c0e1ee56652127173ece2f7fa0de43820
Hugo Prenat <hugo.prenat@epitech.eu>
2016-01-26 01:47:50 +0100

    Implementation of the navigation drawer, it has to be improved

--- app/src/main/java/android/dorianamouroux/fr/epiandroid/HomeActivity.java
+++ app/src/main/java/android/dorianamouroux/fr/epiandroid/HomeActivity.java
@@ -1,13 +1,44 @@
 package android.dorianamouroux.fr.epiandroid;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.license:
+                        break;
+                    // ICI DODO !!!!
+                }
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
