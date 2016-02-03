c1077283eb521117961e84a229ade0e9758acc68
Hugo Prenat <hugo.prenat@epitech.eu>
2016-02-03 01:07:26 +0100

    Add Schedule that doesn't work

--- app/src/main/java/android/dorianamouroux/fr/epiandroid/adapter/ActivityAdapter.java
+++ app/src/main/java/android/dorianamouroux/fr/epiandroid/adapter/ActivityAdapter.java
@@ -1,11 +1,22 @@
-package android.dorianamouroux.fr.epiandroid.adaptater;
+package android.dorianamouroux.fr.epiandroid.adapter;
 
-import android.content.Context;
 import android.dorianamouroux.fr.epiandroid.R;
 import android.dorianamouroux.fr.epiandroid.item.Activity;
 
-import android.widget.ArrayAdapter;
+import android.util.Log;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.content.Context;
+
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,8 +24,174 @@ import java.util.List;
  */
 public class ActivityAdapter extends ArrayAdapter<Activity> {
 
-    public ActivityAdapter(Context context, List<Activity> activityList) {
-        super(context, R.layout.activity_home, activityList);
+    public ActivityAdapter(Context context, int resource, List<Activity> objects) {
+        super(context, resource, objects);
     }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater vi = LayoutInflater.from(getContext());
+            view = vi.inflate(R.layout.activity_adapter, null);
+        }
+
+        Log.v("Acitvity Adapter", "Position = " + Integer.toString(position));
+        Activity act = getItem(position);
+        if (act != null) {
+            TextView room = (TextView) view.findViewById(R.id.room);
+            TextView title = (TextView) view.findViewById(R.id.title);
+            TextView time = (TextView) view.findViewById(R.id.time);
+            ImageButton button = (ImageButton) view.findViewById(R.id.imageButton);
+
+            if (room != null) {
+                room.setText(act.getRoom());
+            }
+            if (title != null) {
+                title.setText(act.getTitle());
+            }
+            if (time != null) {
+                String start = act.getHourStart();
+                String end = act.getHourEnd();
+                time.setText(start + " - " + end);
+            }
+        }
+
+        return view;
+    }
+
+//    private Context                 _context;
+//    private List<Activity>          _listActivity;
+//    private int                     _resourceId;
+//
+//    private final Context context;
+//    private final String[] values;
+//
+//    public ActivityAdapter(Context context, int resourceId, List<Activity> values) {
+//        super(context, resourceId, values);
+//        _context = context;
+//        _listActivity = values;
+//        _resourceId = resourceId;
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        LayoutInflater inflater = (LayoutInflater) context
+//                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//
+//        Activity activity = getItem(position);
+//
+//        TextView titleLabel = (TextView) convertView.findViewById(R.id.adapterPlanningTitle);
+//        TextView scheduleLabel = (TextView) convertView.findViewById(R.id.adapterPlanningSchedule);
+//        TextView roomLabel = (TextView) convertView.findViewById(R.id.adapterPlanningRoom);
+//
+//        titleLabel.setText(activity.getTitle());
+//        scheduleLabel.setText(activity.getSchedule());
+//        String roomName = activity.getRoom();
+//        roomName = roomName.substring(roomName.lastIndexOf("/") + 1);
+//        roomLabel.setText(roomName);
+//
+//        View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
+//        TextView textView = (TextView) rowView.findViewById(R.id.label);
+//        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+//        textView.setText(values[position]);
+//        // change the icon for Windows and iPhone
+//        String s = values[position];
+//        if (s.startsWith("iPhone")) {
+//            imageView.setImageResource(R.drawable.no);
+//        } else {
+//            imageView.setImageResource(R.drawable.ok);
+//        }
+//
+//        return rowView;
+//    }
+//}
+
+
+//
+//    public ActivityAdapter(Context context, int resourceId, ArrayList<Activity> listActivity) {
+//        super(context, resourceId, listActivity);
+//        try {
+//            _context = context;
+//            _listActivity = listActivity;
+//            _resourceId = resourceId;
+//
+//            _inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//
+//        } catch (Exception e) {
+//            Log.e("Activity Adapter", "error", e);
+//        }
+//    }
+//
+
+//    public int getCount() {
+//        return _listActivity.size();
+//    }
+//
+//    public Activity getItem(Activity position) {
+//        return position;
+//    }
+//
+//    public long getItemId(int position) {
+//        return position;
+//    }
+//
+//    public static class ViewHolder {
+//        public TextView display_name;
+//        public TextView display_number;
+//
+//    }
+
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        View vi = convertView;
+//        final ViewHolder holder;
+//        try {
+//            if (convertView == null) {
+//                vi = _inflater.inflate(R.layout.yourlayout, null);
+//                holder = new ViewHolder();
+//
+//                holder.display_name = (TextView) vi.findViewById(R.id.display_name);
+//                holder.display_number = (TextView) vi.findViewById(R.id.display_number);
+
+//                if (planningItem != null) {
+//                    TextView titleLabel = (TextView) v.findViewById(R.id.adapterPlanningTitle);
+//                    TextView scheduleLabel = (TextView) v.findViewById(R.id.adapterPlanningSchedule);
+//                    TextView roomLabel = (TextView) v.findViewById(R.id.adapterPlanningRoom);
+//                    Button btnToken = (Button) v.findViewById(R.id.btnToken);
+//
+//                    btnToken.setTag(position);
+//
+//                    if (titleLabel != null) {
+//                        titleLabel.setText(planningItem.getTitle());
+//                    }
+//                    if (scheduleLabel != null) {
+//                        scheduleLabel.setText(planningItem.getSchedule());
+//                    }
+//
+//                    if (roomLabel != null) {
+//                        String roomName = planningItem.getRoom().getCode();
+//                    }
+//
+//                    roomName = roomName.substring(roomName.lastIndexOf("/") + 1);
+//                }
+//
+//                vi.setTag(holder);
+//            }
+//            else {
+//                holder = (ViewHolder) vi.getTag();
+//            }
+//
+//
+//
+//            holder.display_name.setText(_listActivity.get(position).getTitle());
+//            holder.display_number.setText(_listActivity.get(position).getRoom());
+//
+//
+//        } catch (Exception e) {
+//            Log.e("Activity Adapter", "error", e);
+//        }
+//        return vi;
+//    }
+
 
 }
--- app/src/main/java/android/dorianamouroux/fr/epiandroid/fragment/ScheduleFragment.java
+++ app/src/main/java/android/dorianamouroux/fr/epiandroid/fragment/ScheduleFragment.java
@@ -1,5 +1,6 @@
 package android.dorianamouroux.fr.epiandroid.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import android.support.annotation.Nullable;
@@ -12,10 +13,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.dorianamouroux.fr.epiandroid.R;
-import android.dorianamouroux.fr.epiandroid.IntraAPI;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import android.dorianamouroux.fr.epiandroid.R;
+import android.dorianamouroux.fr.epiandroid.IntraAPI;
+import android.dorianamouroux.fr.epiandroid.adapter.ActivityAdapter;
+import android.dorianamouroux.fr.epiandroid.item.Activity;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -28,21 +37,49 @@ import cz.msebera.android.httpclient.Header;
 
 public class ScheduleFragment extends Fragment {
 
-    private ListView _activityList;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
-    @Nullable
+    private ListView            _activityList;
+    private ActivityAdapter     _adapter;
+    private ArrayList<Activity>      _listActivity  = new ArrayList<>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.schedule_fragment, container, false);
-
         _activityList = (ListView) view.findViewById(R.id.activityList);
 
+        Log.v("Lol : ", "Avant");
         IntraAPI.planning("2016-01-27", "2016-01-27", new JsonHttpResponseHandler() {
+
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Log.v("Lol : ", "je rentre");
+                JSONArray json;
                 try {
-                    System.out.println(response.toString());
-                    addSchedule(response.getJSONArray("items"));
+                    json = new JSONArray(response.toString());
+                    int i = 1;
+                    JSONObject tmp;
+//                    for (int i = 0; i < json.length(); ++i) {
+                    if ((tmp = json.getJSONObject(i)) != null) {
+                            if (tmp.has("module_registered") && tmp.getBoolean("module_registered")) {
+                                Log.v("Lol : ", "Pendant " + Integer.toString(i));
+                                Activity item = new Activity();
+                                item.setTitle(tmp.getString("acti_title"));
+                                item.setStart(tmp.getString("start"));
+                                item.setEnd(tmp.getString("end"));
+                                item.setCodeacti(tmp.getString("codeacti"));
+                                item.setCodeinstance(tmp.getString("codeinstance"));
+                                item.setCodeevent(tmp.getString("codeevent"));
+                                item.setCodemodule(tmp.getString("codemodule"));
+                                item.setScolaryear(tmp.getString("scolaryear"));
+                                item.setRegistered(tmp.getString("event_registered"));
+                                _listActivity.add(item);
+                            }
+                        }
+//                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -50,21 +87,22 @@ public class ScheduleFragment extends Fragment {
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                Log.v("Fail request trombi", String.valueOf(errorResponse));
+                Log.v("Schedule :", String.valueOf(errorResponse));
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String response, Throwable throwable) {
+                Log.i("Schedule ", response);
             }
         });
 
+        Log.v("Lol : ", "Après");
+
+        _adapter = new ActivityAdapter (view.getContext(), R.layout.activity_adapter, _listActivity);
+        _activityList.setAdapter(_adapter);
+//        ListAdapter planningSwipeAdapter = new PlanningSwipeAdapter(rootView.getContext(), R.layout.planning_item, items);
+
         return view;
-    }
-
-    private void addSchedule(JSONArray scheduleArray)
-    {
-        try {
-            JSONObject scheduleItem = scheduleArray.getJSONObject(0);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 
 }
